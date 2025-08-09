LineageOS/android_device_xiaomi_sweet

sweet: patch com.vidhance.node.eis.so with libui-v34
Change-Id: If8651ef34c3212d5fa6f1314e46346406b6761a9

extract-files.py
@@ -37,6 +37,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
+    'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
     ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')

galadriel-at-22-00/device_xiaomi_sweet

Signed-off-by: therealmharc <therealmharc@gmail.com>

extract-files.py
@@ -41,6 +41,8 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
         .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
         .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
+    'vendor/lib64/camera/components/com.vidhance.node.eis.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
     ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')