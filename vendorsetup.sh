AxionAOSP-devices/android_device_xiaomi_sweet

sweet: AxionAOSP bringup
Change-Id: Ie815cef8d19246786e8744faf55944f6f014c880
Signed-off-by: eidawon <projecteidawon@gmail.com>

vendorsetup.sh
@@ -1,0 +1,19 @@
+#!/bin/bash
+
+step() {
+  echo -e "\n==> $1"
+}
+
+# Clone Signing Keys
+step "Setting up signing keys..."
+(
+  if [ -d vendor/lineage-priv/keys ]; then
+    echo "Removing existing signing keys..."
+    rm -rf vendor/lineage-priv/keys
+  fi
+
+  echo "Cloning fresh signing keys..."
+  git clone git@github.com:Eida-at-22-00/keys.git -b lineage-23.0 vendor/lineage-priv/keys
+)
+
+echo -e "\nAll steps completed successfully!"

galadriel-at-22-00/device_xiaomi_sweet

vendorsetup.sh
@@ -1,0 +1,19 @@
+#!/bin/bash
+
+step() {
+  echo -e "\n==> $1"
+}
+
+# Clone Signing Keys
+step "Setting up signing keys..."
+(
+  if [ -d vendor/lineage-priv/keys ]; then
+    echo "Removing existing signing keys..."
+    rm -rf vendor/lineage-priv/keys
+  fi
+
+  echo "Cloning fresh signing keys..."
+  git clone git@github.com:galadriel-at-22-00/keys.git -b lineage-23.0 vendor/lineage-priv/keys
+)
+
+echo -e "\nAll steps completed successfully!"